01fb9efae519cc14ad481fdf178515c46bed480c
jaunn-victorr <joao.vitor.ptal@gmail.com>
2021-10-22 11:33:14 +0000

    reader atualizado

--- includes/reader.php
+++ includes/reader.php
@@ -6,5 +6,22 @@
     <title>BIBLIOTECA - <?= $_title ?></title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-ligth bg-ligth">
+        <div class="container">
+            <a class="navbar-brand" href="/index.php">Biblioteca</a>
+            <button class="navbar-toggler" type="button"data-bs-toggle="colapse" data-bd-target="#navbarSupportedContent" aria-expanded="false">
+                
+                <span class="navbar-toggler-icon"></span>
+                
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav".
+                <li class="nav-item">
+                    <a class="nav-link" href="/index.php">Gêneros</a>
+                </li>
+            </ul>
+'        </div>
+    </div>
+</nav>
     
     <main class="container">
